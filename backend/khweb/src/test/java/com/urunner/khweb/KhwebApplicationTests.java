3817b3c72eb25077309dfd4ce88445703d1c5905
silenc3502 <gcccompil3r@gmail.com>
2021-10-13 16:59:58 +0900

    Init Repository(저장소 초기 설정 완료!)

---
+++ backend/khweb/src/test/java/com/urunner/khweb/KhwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urunner.khweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
