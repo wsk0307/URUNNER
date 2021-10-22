691268b94bf8ab8f8a6cf265023d62867cb29240
seungree Jeon <82028073+JEONSEUNGREE@users.noreply.github.com>
2021-10-22 16:39:21 +0900

    "security_dependencies" (#16)
    * "security_cors_setting"
    
    * vue_login_method
    
    * Delete common.js
    
    delete test
    
    * "security_jwt"
    
    * "security_jwt"
    
    * "security_jwt_dependencies"
    
    Co-authored-by: 최승현 <83811793+KH-CSH@users.noreply.github.com>

--- backend/khweb/src/main/java/com/urunner/khweb/config/SecurityConfig.java
+++ backend/khweb/src/main/java/com/urunner/khweb/config/SecurityConfig.java
@@ -1,44 +1,57 @@
 package com.urunner.khweb.config;
 
-import org.springframework.context.annotation.Bean;
+
+import com.urunner.khweb.filter.CustomAuthenticationFilter;
+import com.urunner.khweb.filter.CustomAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(
-        securedEnabled = true,
-        jsr250Enabled = true,
-        prePostEnabled = true
-)
+
+@Configuration @EnableWebSecurity @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager());
+
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
         http
+                .csrf().disable()
                 .cors()
                 .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+                .authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh").permitAll()
                 .and()
-                .csrf()
-                .disable()
-                .formLogin()
-                .disable()
-                .httpBasic()
-                .disable();
+                .authorizeRequests().antMatchers(HttpMethod.POST, "/api/users/**").hasAnyAuthority("ROLE_USER")
+                .and()
+                .authorizeRequests().antMatchers("/memberManagement/**").permitAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
 
+        http
+                .addFilter(customAuthenticationFilter)
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
+
 }
