70efeed3ba774032a0db63d485c04a25ae4956a6
baomei1996 <65599030+baomei1996@users.noreply.github.com>
2021-10-27 13:14:44 +0900

    [안보미] - 프론트엔드 메인페이지 구현 메뉴바, 알림창 추가구현 (#26)
    * 안보미 프론트앤드 로그인 회원가입 페이지 구현
    
    * [안보미] - 프론트엔드 메인페이지 구현

---
+++ frontend/src/components/headerLoginMenu/OpenMainMenuCart.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card color="primary" width="500">
+    <v-tabs fixed-tabs slider-color="primary">
+      <v-tab v-for="(item, index) in items" :key="index">
+        {{ item }}
+      </v-tab>
+        <v-tab-item>
+          <v-card>
+            <v-list>
+              <v-list-item-group color="primary">
+                <v-list-item v-for="(item, i) in LectureLists" :key="i" class="mb-2">
+                  <v-img :src="item.image" max-height="89" max-width="123"/>
+                  <v-list-item-content class="ml-2">
+                    <v-list-item-title><h4>{{ item.title }}</h4></v-list-item-title>
+                    <v-list-item-subtitle v-text="item.price" class="text-right pa-3"/>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+            <v-list class="ma-0 pa-0">
+              <v-btn color="primary" x-large block>수강 바구니로 이동</v-btn>
+            </v-list>
+        </v-card>
+       </v-tab-item>
+
+        <v-tab-item>
+          <v-card>
+            <v-list>
+              <v-list-item-content>
+                <v-list-item-subtitle><p class="text-center">위시 리스트가 없습니다. </p></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list>
+            <v-list class="ma-0 pa-0">
+              <v-btn color="primary" x-large block>위시 리스트로 이동</v-btn>
+            </v-list>
+        </v-card>
+       </v-tab-item>
+    </v-tabs>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        tab: null,
+        items: [
+          '수강바구니', '위시리스트'
+        ],
+        LectureLists: [
+        { title: '스스로 구축하는 AWS 클라우드 네트워크 - 기본편', price: '₩42,000', image: 'https://cdn.inflearn.com/public/courses/327724/cover/e6ee6969-4d60-4b64-8401-11c5ed0ad288/327724-eng.png' },
+        { title: '스스로 구축하는 AWS 클라우드 네트워크 - 기본편', price: '₩42,000', image: 'https://cdn.inflearn.com/public/courses/327724/cover/e6ee6969-4d60-4b64-8401-11c5ed0ad288/327724-eng.png' },
+        { title: '스스로 구축하는 AWS 클라우드 네트워크 - 기본편', price: '₩42,000', image: 'https://cdn.inflearn.com/public/courses/327724/cover/e6ee6969-4d60-4b64-8401-11c5ed0ad288/327724-eng.png' },
+      ],
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,114 +10,52 @@ import MyPageStatus from '@/views/mypage/MyPageStatus.vue'
 Vue.use(VueRouter)
 
 export default new VueRouter({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name:'MainPage',
-      component: () => import('../views/MainPage.vue'),
-    },
-    {
-      path: '/memberRegister',
-      component: () => import('../views/MemberRegisterPage.vue')
-    },
-    {
-      path: '/memberProfile',
-      component: () => import('../views/MemberProfilePage.vue')
-    },
-    // 회원 탈퇴
-    {
-      path: '/member/:memberNo',
-      name: 'LeaveMemberPage',
-      components: {
-        default: LeaveMemberPage
-      },
-      props: {
-        default: true
-      }
-    },
-    //myPage
-    {
-      path: '/my-page-status',
-      name: 'MyPageStatus',
-      components: {
-        default: MyPageStatus
-      },
-      props: {
-        default: true
-      }
-    },
-    {
-      path: '/mypage',
-      component: () => import("../views/mypage/Mypage.vue"),
-      children: [
-    {
-      path: '/wishlist',
-      component: () => import("../components/enrolment/WishList.vue")
-    }
-    },
-    {
-      path: '/cart',
-      component: () => import("../components/enrolment/Cart.vue")
-    },
-  ]
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'MainPage',
+            component: () => import ('../views/MainPage.vue')
+        }, {
+            path: '/memberRegister',
+            component: () => import ('../views/MemberRegisterPage.vue')
+        }, {
+            path: '/memberProfile',
+            component: () => import ('../views/MemberProfilePage.vue')
+        },
+        // 회원 탈퇴
+        {
+            path: '/member/:memberNo',
+            name: 'LeaveMemberPage',
+            components: {
+                default: LeaveMemberPage
+            },
+            props: {
+                default: true
+            }
+        },
+        //myPage
+        {
+            path: '/my-page-status',
+            name: 'MyPageStatus',
+            components: {
+                default: MyPageStatus
+            },
+            props: {
+                default: true
+            }
+        }, {
+            path: '/mypage',
+            component: () => import ("../views/mypage/Mypage.vue"),
+            children: [
+                {
+                    path: '/wishlist',
+                    component: () => import ("../components/enrolment/WishList.vue")
+                }, {
+                    path: '/cart',
+                    component: () => import ("../components/enrolment/Cart.vue")
+                }
+            ]
+        }
+    ]
 })
-
-
-import axios from "axios";
-
-
-// axios.defaults.headers.common['Authorization'] = Vue.$cookies.get("ACCESS_TOKEN");
-
-// Add a request interceptor
-
-// 요청전 전처리
-// 주의할점 두번 요청할경우 문제발생
-axios.interceptors.request.use(function (config) {
-    // Do something before request is sent
-
-    // if(!Vue.$cookies.get("ACCESS_TOKEN")) {
-    //   return Promise.reject("No access token set")
-    // }
-    if(!Vue.$cookies.get("ACCESS_TOKEN")) {
-      config.headers.Authorization = Vue.$cookies.get("REFRESH_TOKEN");
-    }else{
-      config.headers.Authorization = Vue.$cookies.get("ACCESS_TOKEN");
-    }
-    alert("요청")
-
-    return config;
-  }, function (error) {
-    // Do something with request error
-
-    return Promise.reject(error);
-  });
- 
-// Add a response interceptor
-
-// 응답받기전 전처리
-axios.interceptors.response.use(function (response) {
-
-    // Do something with response data
-    return response;
-  }, function (error) {
-
-  alert(error.response.data.error)
-
-  // let getRefresh = error.response.data.error
-
-  // if (getRefresh.includes("The Token has expired")){
-  //     refreshToken()
-  // }
-
-
-
-    // alert(res.response.data.error)
-
-    // Do something with response error
-    return Promise.reject(error);
-  });
-
-
-
-// //   https://www.npmjs.com/package/axios
