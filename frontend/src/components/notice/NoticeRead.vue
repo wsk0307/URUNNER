f1992e29c1b3412cfbd76002ba6e94ff16ce3a01
YoonYeongKim3 <93516152+YoonYeongKim3@users.noreply.github.com>
2021-11-01 21:16:47 +0900

    [김윤영] 공지사항 게시판 수정 (#50)
    * [김윤영] 공지사항 게시판 수정
    
    * [김윤영] 공지사항 게시판 수정
    
    Co-authored-by: YoonYeongKim1 <03y20y@naver.com>

--- frontend/src/components/notice/NoticeRead.vue
+++ frontend/src/components/notice/NoticeRead.vue
@@ -1,21 +1,29 @@
 <template>
-<div>
-    <div class="column is-10">
-            <div class="columns post_header">
-              <div class="column is-full">
-                  <h1 class="post-title">{{notice.title}}</h1>
+  <div>
+      <div class="column is-10">
+        <div class="columns post_header">
+          <div class="column is-full">
+              <h1 class="제목" style="padding: 50px 0px 20px 0px;">{{notice.title}}</h1>
+          </div>
+          <hr width="80%">
+          <div class="본문" align="left">
+              <div class="날짜" style="padding: 10px;">
+                <strong>{{new Date(notice.upDate).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"}).toString().substr(0, 11)}}</strong>
+              </div>      
+              <div class="작성자" style="padding: 10px;">
+                <strong>작성자 : {{notice.writer}}</strong>
+              </div>  
+              <div class="내용" style="padding: 10px;" v-html="content">
+                <p>{{ notice.content }}</p>
               </div>
-            </div>
-            <div><strong>작성자 : {{notice.writer}}</strong></div>
-            <span class="date">{{new Date(notice.regDate).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"}).toString().substr(0, 11)}}</span>
-            <div class="post-body" v-html="content">
-				<p>{{ notice.content }}</p>
-			</div>
-    </div>
-</div>
+          </div>
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
+
 export default {
     name: 'NoticeRead',
     props: {
@@ -34,7 +42,7 @@ export default {
 
 <style scoped>
 
-.글작성 {
-  padding:0px 200px 0px 200px;
+.본문 {
+  padding:0px 230px 0px 230px;
 }
 </style>
