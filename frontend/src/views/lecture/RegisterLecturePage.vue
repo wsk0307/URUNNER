816f48ce9a0a96d4ad6b7efb5d5bfd23e5fa8ed8
baomei1996 <65599030+baomei1996@users.noreply.github.com>
2021-11-01 19:30:00 +0900

    [안보미] 프론트 간편 로그인 버튼 & 강의 등록 시스템 (#48)

---
+++ frontend/src/views/lecture/RegisterLecturePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <register-lecture-form></register-lecture-form>  
+</template>
+
+<script>
+import RegisterLectureForm from '@/components/lecture/RegisterLectureForm.vue'
+export default {
+  components: {
+    RegisterLectureForm
+  }
+
+}
+</script>
+
+<style>
+
+</style>
