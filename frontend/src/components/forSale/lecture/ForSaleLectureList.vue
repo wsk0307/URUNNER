7643a71ba96eb83054a790edd2f526b876946c4c
CSH <endras132@naver.com>
2021-11-24 15:10:54 +0900

    Update ForSaleLectureList.vue

--- frontend/src/components/forSale/lecture/ForSaleLectureList.vue
+++ frontend/src/components/forSale/lecture/ForSaleLectureList.vue
@@ -133,7 +133,7 @@
                                     <div class="btn-plus2">
                                         <span draggable="false">
                                             <b @click="goPage(mob.id)"> 
-                                                <div style="font-size:13px;text-align:center">{{ mob.desc }} 대부분 LEFT OUTER JOIN을 많이 사용하지만 상황에 따라서 RIGHT OUTER JOIN을 사용할 수 있으니 개념을 꼭 이해하고 있어야 한다.</div>
+                                                <div class="desc_text">{{ mob.desc }}</div>
                                             </b>
                                             <div class="align-right" >
                                                 <v-icon :color="wish[index] ? 'red' : 'white'" class="d-block pa-1" @click.prevent="toggleHeartBtn(mob, index)">
@@ -1115,5 +1115,16 @@ a:hover { text-decoration:none !important }
   transform: scale(1.5);
   transition: transform 1s;
   filter: brightness(70%);
+}
+.desc_text {
+    font-size:13px;
+    text-align:center;
+
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    line-height: 22px;
+    -webkit-line-clamp: 5; /* 표시하고자 하는 라인 수 */
+    -webkit-box-orient: vertical;
 }
 </style>
